1406ac4161b1d54baa165949a906f58dbe96e267
Dimitri Roche <dimroc@gmail.com>
2015-05-12 08:37:44 -0400

    Refactor listener/ into producers/

--- geoevent/geoevent.go
+++ geoevent/geoevent.go
@@ -4,48 +4,50 @@ import (
 	"github.com/azr/anaconda"
 )
 
-type geoEvent struct {
-	GeoJson geoJson `json:"geojson"`
+type GeoEvent struct {
+	GeoJson GeoJson `json:"geojson"`
 	Type    string  `json:"type"`
 	Payload string  `json:"payload"`
 }
 
-type geoJson interface {}
+type GeoJson interface{}
 
-type point struct {
+type Point struct {
 	Coordinates [2]float64 `json:"coordinates"` // Coordinate always has to have exactly 2 values
 	Type        string     `json:"type"`
 }
 
-type boundingBox struct {
+type BoundingBox struct {
 	Coordinates [][][]float64 `json:"coordinates"`
 	Type        string        `json:"type"`
 }
 
-func geoJsonFromPoint(t anaconda.Tweet) geoJson {
-	return &point{
+func geoJsonFromPoint(t anaconda.Tweet) GeoJson {
+	return &Point{
 		Coordinates: t.Coordinates.Coordinates,
 		Type:        t.Coordinates.Type,
 	}
 }
 
-func geoJsonFromBoundingBox(t anaconda.Tweet) geoJson {
-	return &boundingBox{
+func geoJsonFromBoundingBox(t anaconda.Tweet) GeoJson {
+	return &BoundingBox{
 		Coordinates: t.Place.BoundingBox.Coordinates,
 		Type:        t.Place.BoundingBox.Type,
 	}
 }
 
-func NewFromTweet(t anaconda.Tweet) *geoEvent {
+func NewFromTweet(t anaconda.Tweet) *GeoEvent {
 	if t.Coordinates != nil {
-		return &geoEvent{
+		return &GeoEvent{
 			GeoJson: geoJsonFromPoint(t),
-			Type:    "tweet", Payload: t.Text,
+			Type:    "tweet",
+			Payload: t.Text,
 		}
 	} else {
-		return &geoEvent{
+		return &GeoEvent{
 			GeoJson: geoJsonFromBoundingBox(t),
-			Type:    "tweet", Payload: t.Text,
+			Type:    "tweet",
+			Payload: t.Text,
 		}
 	}
 }
