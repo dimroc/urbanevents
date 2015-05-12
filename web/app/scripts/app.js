cdfa5a65272d90a5c51659856eea2b4ce6d2f8e8
Dimitri Roche <dimroc@gmail.com>
2015-05-12 13:24:39 -0400

    Add routes and react-bootstrap button group

--- web/app/scripts/app.js
+++ web/app/scripts/app.js
@@ -1,9 +1,45 @@
-
 var React = window.React = require('react'),
+    Router = require('react-router'),
     Timer = require("./ui/Timer"),
     PusherEvents = require("./components/PusherEvents"),
+    ReactBootstrap = require('react-bootstrap'),
     mountNode = document.getElementById("app");
 
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
 
-React.render(<PusherEvents />, mountNode);
+var ButtonGroup = ReactBootstrap.ButtonGroup;
+var Button = ReactBootstrap.Button;
 
+//http://getbootstrap.com/components/#btn-groups-justified
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <header className="row">
+          <div className="col-xs-12">
+            <ButtonGroup justified>
+              <Button href="/#">Map</Button>
+              <Button href="/#/events">Events</Button>
+            </ButtonGroup>
+          </div>
+        </header>
+
+        {/* this is the important part */}
+        <RouteHandler/>
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route name="app" handler={App} path="/">
+    <Route name="events" handler={PusherEvents}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, mountNode);
+});
