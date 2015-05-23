7f460c4732ebbc1d7fe5e49850da7acdaad4ba6c
Dimitri Roche <dimroc@gmail.com>
2015-05-22 22:07:05 -0400

    Clean up twitterlistener/ and pusher/ main.go to use ENV

--- cityservice/pusher/main.go
+++ cityservice/pusher/main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"flag"
 	"github.com/dimroc/urban-events/cityservice/cityrecorder"
-	"github.com/dimroc/urban-events/cityservice/flagvalidator"
 	"os"
 )
 
-var (
-	pusherAppId  = flag.String("appid", "", "Pusher App Id")
-	pusherKey    = flag.String("key", "", "Pusher Key")
-	pusherSecret = flag.String("secret", "", "Pusher Secret")
-)
-
 func main() {
-	flag.Parse()
-	flags := []string{"appid", "key", "secret"}
-	flagvalidator.ValidateFlags(flags)
-
-	pusher := cityrecorder.NewPusher(*pusherAppId, *pusherKey, *pusherSecret)
+	pusher := cityrecorder.NewPusherFromURL(os.Getenv("PUSHER_URL"))
 	pusher.Listen(os.Stdin)
 }
--- cityservice/twitterlistener/main.go
+++ cityservice/twitterlistener/main.go
@@ -1,41 +1,18 @@
 package main
 
 import (
-	"flag"
 	"github.com/dimroc/urban-events/cityservice/cityrecorder"
-	"github.com/dimroc/urban-events/cityservice/flagvalidator"
 	"log"
-)
-
-var (
-	consumerKey    = flag.String("consumer-key", "", "Twitter Consumer Key")
-	consumerSecret = flag.String("consumer-secret", "", "Twitter Consumer Key")
-	token          = flag.String("token", "", "Twitter Access Token")
-	tokenSecret    = flag.String("token-secret", "", "Twitter Token Secret")
+	"os"
 )
 
 func main() {
-	flag.Parse()
-	flags := []string{"consumer-key", "consumer-secret", "token", "token-secret"}
-	flagvalidator.ValidateFlags(flags)
-
-	recorder := cityrecorder.TweetRecorder{
-		ConsumerKey:    *consumerKey,
-		ConsumerSecret: *consumerSecret,
-		Token:          *token,
-		TokenSecret:    *tokenSecret,
-	}
-
-	//city := cityrecorder.City{
-	//Key:       "nyc",
-	//Display:   "New York City",
-	//Locations: "-74.3,40.462,-73.65,40.95",
-	//}
-
-	//cities := []cityrecorder.City{city}
-	//settings := cityrecorder.Settings{
-	//Cities: cities,
-	//}
+	recorder := cityrecorder.NewTweetRecorder(
+		os.Getenv("TWITTER_CONSUMER_KEY"),
+		os.Getenv("TWITTER_CONSUMER_SECRET"),
+		os.Getenv("TWITTER_TOKEN"),
+		os.Getenv("TWITTER_TOKEN_SECRET"),
+	)
 
 	settings, err := cityrecorder.LoadSettings()
 	if err != nil {
