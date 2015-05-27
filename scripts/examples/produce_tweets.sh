84cbc6bacf92cbc3a68f7426b46b32203ea04cde
Dimitri Roche <dimroc@gmail.com>
2015-05-27 11:42:24 -0400

    Remove unused scripts/ folder

--- scripts/examples/produce_tweets.sh
+++
@@ -1,3 +0,0 @@
-go run twitterlistener/main.go --consumer-key=<CONSUMER KEY> --consumer-secret=<CONSUMER SECRET> \
-                            --token=<TOKEN> --token-secret=<TOKEN SECRET> \
-                           | go run pusher/main.go --appid=<PUSHER APP ID> --key=<PUSHER KEY> --secret=<PUSHER SECRET>
