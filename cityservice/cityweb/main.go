f258bb93ae6757a4fa35a999fbaa51c70b26101f
Dimitri Roche <dimroc@gmail.com>
2015-07-26 12:55:50 -0400

    Complete groundwork for cityweb

--- cityservice/cityweb/main.go
+++ cityservice/cityweb/main.go
@@ -1,39 +1,47 @@
-package citysearch
+package main
 
 import (
 	"github.com/codegangsta/negroni"
-	"github.com/dimroc/urbanevents/cityservice/cityrecorder"
+	"github.com/dimroc/urbanevents/cityservice/citylib"
 	. "github.com/dimroc/urbanevents/cityservice/utils"
 	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	"github.com/unrolled/render"
+	"net/http"
+	"os"
 )
 
 const (
 	CTX_ELASTIC_CONNECTION_KEY = "city.elasticconnection"
 )
 
+var (
+	settingsFilename = GetenvOrDefault("CITYSERVICE_SETTINGS", "../config/nyc.json")
+)
+
 func main() {
 	Logger.Info("Running in " + GO_ENV)
+	settings, settingsErr := citylib.LoadSettings(settingsFilename)
+	Check(settingsErr)
 
-	elastic := cityrecorder.NewElasticConnection(os.Getenv("ELASTICSEARCH_URL"))
+	elastic := citylib.NewElasticConnection(os.Getenv("ELASTICSEARCH_URL"))
 
 	router := mux.NewRouter()
 	apiRoutes := router.PathPrefix("/api/v1").Subrouter()
 	apiRoutes.HandleFunc("/cities", CitiesHandler).Methods("GET")
-	apiRoutes.HandleFunc("/cities/{city}", CityHandler).Methods("GET")
+	apiRoutes.HandleFunc("/cities/{city}", citylib.CityHandler).Methods("GET")
 
 	n := negroni.Classic()
 	n.Use(cors.Default())
-	n.Use(cityrecorder.SettingsMiddleware(settings))
-	n.Use(cityrecorder.ElasticMiddleware(elastic))
+	n.Use(citylib.SettingsMiddleware(settings))
+	n.Use(citylib.ElasticMiddleware(elastic))
 	n.UseHandler(context.ClearHandler(router))
-	n.Run()
+	n.Run(":5000")
 }
 
 func CitiesHandler(w http.ResponseWriter, req *http.Request) {
 	r := render.New(render.Options{IndentJSON: true})
-	settings := GetSettings(req)
+	settings := citylib.GetSettings(req)
 	r.JSON(w, http.StatusOK, settings.Cities)
 }
