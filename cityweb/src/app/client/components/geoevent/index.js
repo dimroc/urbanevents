dac6f4dfb27b55f50b70ef5a0094bbbd78e21d95
Dimitri Roche <dimroc@gmail.com>
2015-12-20 16:56:50 -0500

    Show geoevent images and introduce bootleg styles

--- cityweb/src/app/client/components/geoevent/index.js
+++ cityweb/src/app/client/components/geoevent/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import styles from './styles';
 
 export default class Geoevent extends Component {
   render() {
@@ -7,10 +8,11 @@ export default class Geoevent extends Component {
     var image = null
     console.log(geoevent.mediaUrl)
     if (geoevent.mediaType != "text") {
-      image = <img src={geoevent.medaUrl}/>
+      image = <img src={geoevent.mediaUrl}/>
     }
 
-    return <div>
+    let className = styles.geoevent + " " + geoevent.mediaType;
+    return <div className={className}>
       <div>{geoevent.text}</div>
       {image}
     </div>
@@ -21,9 +23,9 @@ Geoevent.propTypes = {
   geoevent: React.PropTypes.object.isRequired
 };
 
-      //<div className="full-name">{geoevent.fullName}</div>
-      //<div className="text">{geoevent.text}</div>
-      //<div className="media-url"><a href={geoevent.mediaUrl} target="_blank">{geoevent.mediaUrl}</a></div>
-      //<div className="neighborhoods">{geoevent.neighborhoods}</div>
-      //<div className="service">{geoevent.service}</div>
-      //<div className="created-at">{geoevent.createdAt}</div>
+//<div className="full-name">{geoevent.fullName}</div>
+//<div className="text">{geoevent.text}</div>
+//<div className="media-url"><a href={geoevent.mediaUrl} target="_blank">{geoevent.mediaUrl}</a></div>
+//<div className="neighborhoods">{geoevent.neighborhoods}</div>
+//<div className="service">{geoevent.service}</div>
+//<div className="created-at">{geoevent.createdAt}</div>
