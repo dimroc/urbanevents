301ff5bd10a653d7770470c62d2920c59200235a
Dimitri Roche <dimroc@gmail.com>
2015-12-23 12:43:16 -0500

    Simplify store creation

--- cityweb/src/app/client/store.js
+++ cityweb/src/app/client/store.js
@@ -3,22 +3,11 @@ import thunk from 'redux-thunk'
 import { devTools, persistState } from 'redux-devtools';
 import rootReducer from '#app/reducers/index.js';
 
-let finalCreateStore;
 const middleware = applyMiddleware(thunk);
 
-if (process.env.NODE_ENV === 'production') {
-  finalCreateStore = compose(middleware)(reduxCreateStore).bind(null, rootReducer);
-} else {
-  try {
-    finalCreateStore = compose(
-      middleware,
-      devTools(),
-      persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-    )(reduxCreateStore).bind(null, rootReducer);
-    console.log('dev tools added');
-  } catch (e) {
-    finalCreateStore = compose(middleware)(reduxCreateStore).bind(null, rootReducer);
-  }
-}
+let finalCreateStore = compose(
+  middleware,
+  devTools()
+)(reduxCreateStore).bind(null, rootReducer);
 
 export const createStore = finalCreateStore;
